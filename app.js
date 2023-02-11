f611e84c97be4e38bcbe96e1f3a2894d46a49673
Suyash <=>
2023-02-11 18:05:59 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+var items = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function (req, res) {
+  var today = new Date();
+  
+  var options = {
+
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+  
+  
+
+  res.render("list", { kindOfDay: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
+  
+ 
+
+});
+
+app.listen(3005, function () {
+  console.log("server is started on port 3005");
+});
